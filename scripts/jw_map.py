76a9bb6202f1858684c6087acd7fd89508528b30
Bastien Gorissen <bastien@panopticgame.com>
2018-07-12 15:15:30 +0200

    Extract location map data

--- scripts/jw_map.py
+++ scripts/jw_map.py
@@ -15,7 +15,6 @@ import os
 
 rom = None
 
-
 PALETTES_VALUES = {
     b'\x00': 'Jungle dungeon',
     b'\x01': 'Cave with tracks',
@@ -37,7 +36,8 @@ def read_map_data_header(pointer, bank=0xD):
     rom.seek(bank * 0x4000 + pointer - 0x4000)
     header['width'] = int.from_bytes(rom.read(1), byteorder='little')
     header['height'] = int.from_bytes(rom.read(1), byteorder='little')
-    header['out_of_bounds_tile'] = int.from_bytes(rom.read(1), byteorder='little')
+    header['out_of_bounds_tile'] = int.from_bytes(
+        rom.read(1), byteorder='little')
     header['palette_index'] = int.from_bytes(rom.read(1), byteorder='little')
     header['unknown_1'] = int.from_bytes(rom.read(1), byteorder='little')
     header['unknown_2'] = int.from_bytes(rom.read(1), byteorder='little')
@@ -48,150 +48,138 @@ def read_map_data_header(pointer, bank=0xD):
 
 
 def swap_byte(b):
-    return (((b << 4) & 0xF0) |
-            ((b >> 4) & 0x0F))
+    return (((b << 4) & 0xF0) | ((b >> 4) & 0x0F))
+
+
+def hl_from_bytes(low, high):
+    return high << 8 | low
+
+
+def pointer_from_position(pos, bank):
+    return pos - 0x4000 * (bank - 1)
+
+
+def bytes_from_pointer(pointer):
+    return ((pointer & 0xFF00) >> 8, pointer & 0x00FF)
+
+
+def seek_to_pointer(pointer, bank):
+    rom.seek(pointer + (bank - 1) * 0x4000)
+
+
+def read_bytes(n=1):
+    return int.from_bytes(rom.read(n), byteorder='little')
 
 
 def read_map(index):
     map_data = {}
     map_data_pointer = find_map_data_pointer(index)
-    # print(hex(map_data_pointer))
     map_data['header'] = read_map_data_header(map_data_pointer)
 
     # Position in the rom is now 10 bytes after the header
-    C2A1 = int.from_bytes(rom.read(1), byteorder='little')
-    C2A2 = int.from_bytes(rom.read(1), byteorder='little')
-    FFBB = int.from_bytes(rom.read(1), byteorder='little')
-    FFBC = int.from_bytes(rom.read(1), byteorder='little')
+    C2A1 = read_bytes(1)
+    C2A2 = read_bytes(1)
 
+    # --- Load_Map (0D:4100)
+    FFBB = read_bytes(1)
+    FFBC = read_bytes(1)
+
+    # This is supposed to be FFB2 & FFB1
     HL_ram = rom.tell()
-    # HL_ram = rom.tell() - (0xD - 1) * 0x4000
-    # print(bytes.fromhex(hex(rom.tell() - (0xD - 1) * 0x4000)[2:]))
 
-    # print(hex(HL_ram))
     rom.seek(8, os.SEEK_CUR)
 
-    A = int.from_bytes(rom.read(1), byteorder='little')
-    # print(hex(A))
-
-    A = A * 2
-    # print(hex(rom.tell() - (0xD - 1) * 0x4000))
-    swap = False
+    reading = True
     map_data['decoded'] = []
-    for i in range(8):
-        HL_stack = rom.tell()
-        rom.seek(HL_ram)
 
-        decoding = int.from_bytes(rom.read(1), byteorder='little')
-        swap = not swap
-        if swap:
-            decoding = swap_byte(decoding)
-        decoding = decoding & 0x0F
+    hl_read = rom.tell()
 
-        if FFBB & 0x01 == 0:
-            if FFBB > 0:
-                FFBB -= 1
+    c = 0
+    while reading:
+        # --- Start_Decompressing_Next_Map_Byte (0D:4119)
+        rom.seek(hl_read)
+        byte_a = read_bytes(1)
+        hl_read = rom.tell()
+
+        added = ""
+
+        b_counter = 8
+
+        while b_counter > 0:
+
+            new_a = (byte_a * 2) & 0xFF
+            if byte_a * 2 > 0xFF:
+                byte_a = new_a
+                c += 1
             else:
-                FFBB = 0xFF
-                FFBC -= 1
+                byte_a = new_a
+                HL_ram4126 = rom.tell()
+                rom.seek(HL_ram + (c >> 1))
+                a = read_bytes(1)
+                if c & 0x01 == 0x00:
+                    a = swap_byte(a)
 
-            FFBB = FFBB | FFBC
-            new_byte = swap_byte(decoding)
-            print(hex(new_byte))
-            map_data['decoded'].append(new_byte)
-        else:
-            print(hex(decoding))
-            map_data['decoded'].append(decoding)
+                # --- jr_00d_413d
+                a = a & 0x0F
 
-        if FFBB > 0:
-            FFBB -= 1
-        else:
-            FFBB = 0xFF
-            FFBC -= 1
+                if FFBB & 0x01 == 0:
+                    map_data['decoded'].append(a)
+                else:
+                    # --- 0d:4146
+                    previous_a = map_data['decoded'][-1]
+                    a = swap_byte(previous_a) | a
+                    map_data['decoded'][-1] = a
+                    added += format(a, 'x') + " "
 
-        rom.seek(HL_ram)
+                # --- jr_00d_4154
+                if FFBB > 0:
+                    FFBB -= 1
+                else:
+                    FFBB = 0xFF
+                    FFBC -= 1
+                if FFBB | FFBC == 0:
+                    reading = False
+                else:
+                    c = 0
+                rom.seek(HL_ram4126)
+            b_counter -= 1
 
-
+        # print(added)
     return map_data
 
-# def parse_next_pointer(offset=None):
-#     if offset is not None:
-#         rom.seek(offset)
 
-#     b = rom.read(1)
-#     while b != b'\x21':
-#         b = rom.read(1)
+def prettify_line(line):
+    equivalence = {
+        '56': '[]',
+        '1': '.',
+        'b': ' ',
+        '8': ' ',
+        '2': ' ',
+        '9': ' ',
+        'a': ' ',
+        '4': '-',
+        '6': '|',
+        '5': 'o',
+        '7': 'o',
+        '3': 'o',
+        'f': 'T',
+    }
+    for key in equivalence:
+        line = line.replace(key, equivalence[key])
 
-#     pointer = int.from_bytes(rom.read(2), byteorder='little')
-
-#     rom.seek(0x4000 + pointer)
-
-#     return read_script()
-
-#     return
-#     item_type = rom.read(1)
-#     try:
-#         item_type = ITEM_TYPES[item_type]
-#     except KeyError:
-#         item_type = str(hex(ord(item_type)))
-#     byte2 = rom.read(1)
-#     equipment_effect = rom.read(1)
-#     try:
-#         equipment_effect = EQUIPMENT_EFFECTS[equipment_effect]
-#     except KeyError:
-#         equipment_effect = str(hex(ord(equipment_effect)))
-#     purchase_price = int(''.join(reversed(rom.read(2))).encode('hex'), 16)
-
-#     name = read_item_name()
-
-#     item = {
-#         'name': name,
-#         'item_type': item_type,
-#         'purchase_price': purchase_price,
-#         'equipment_effect': equipment_effect,
-#         'byte2': byte2,
-#         'byte_count': 5 + len(name),
-#     }
-
-#     return item
+    return line
 
 
-# def extract_list(offset):
-#     rom.seek(offset)
-#     print('%17s %-20s %20s %10s : %-12s' % ('Range', 'Type', 'Effect', 'Price',
-#                                             'Name'))
-#     for i in range(87):
-#         item = parse_item()
-#         length = item['byte_count']
-#         print("%s - %s %-20s %20s %10s : %-12s" %
-#               (hex(offset), hex(offset + length), item['item_type'],
-#                item['equipment_effect'], item['purchase_price'], item['name']))
-#         offset += length + 1
+def print_ascii_map(map_data):
+    w = map_data['header']['width']
+    h = map_data['header']['height']
 
-
-# def extract_script(offset):
-#     rom.seek(offset)
-
-#     for i in range(100):
-#         location = int(rom.tell() - offset)
-#         pointer = location.to_bytes(2, byteorder='little')
-#         pointer_print = int.from_bytes(pointer, byteorder='big')
-#         print('{0:#06x} {1:#06x} {2}'.format(location, pointer_print,
-#                                              read_script()))
-
-
-# def look_for_potential_pointers(pointer):
-#     potential_candidates = []
-#     while True:
-#         b = rom.read(1)
-#         if not b:
-#             break
-
-#         if b == b'\x21':
-#             if int.from_bytes(rom.read(2), byteorder='big') == pointer:
-#                 potential_candidates.append(hex(rom.tell() - 2))
-
-#     print(potential_candidates)
+    for row in range(h):
+        line = ""
+        for col in range(int(w / 2)):
+            line += format(map_data['decoded'][col + row * w // 2], '02x')
+        print(prettify_line(line))
+        #print(line)
 
 
 if __name__ == '__main__':
@@ -200,8 +188,18 @@ if __name__ == '__main__':
     rom = open(arguments["<romfile>"], 'rb')
     index = int(arguments["<index>"])
 
-
     if arguments['read']:
         # Example offset : 0xD90F
-        map_data = read_map(index)
-        print(map_data)
+        if index != 999:
+            index = [index]
+        else:
+            index = range(16 * 8)
+
+        for i in index:
+            try:
+                map_data = read_map(i)
+                #print(map_data)
+                print_ascii_map(map_data)
+                print("\n\n")
+            except:
+                pass
