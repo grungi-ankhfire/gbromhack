84bb051f609d58fb5468c5df7182a4aa62b5759a
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2020-04-26 12:07:00 +0200

    Improve utility to insert translation an update table

--- scripts/jw_translation.py
+++ scripts/jw_translation.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-"""Usage: jw_translation.py <romfile> [<tablefile>]
+"""Usage: jw_translation.py [--no-backup] <romfile> <inputfile> <tablefile>
 
 Helping script for manipulating Jungle Wars text.
 
 Arguments
     <romfile>    The rom file to open
-    <start>      The offset in hex to start the dump
-    <end>        The offset in hex to finish the dump
+    <inputfile>  YAML file containing translation
     <tablefile>  Translation table to use
-    <scriptfile> File containing a Unicode script
-    <outputfile> File in which to dump the script
+
+Options
+    --no-backup  Disable the automatic backup before patching
 """
 from docopt import docopt
 from translation_table import TranslationTable
+import pyaml
+import yaml
+import shutil
+import datetime
 
 
 MAX_LENGTH = 17
@@ -21,7 +25,7 @@ MAX_LENGTH = 17
 
 class TextString:
 
-    def __init__(self, pointer_address, text, original_text=""):
+    def __init__(self, pointer_address, text, max_length=MAX_LENGTH):
 
         self.pointer_address = pointer_address
         self.text = text
@@ -29,7 +33,7 @@ class TextString:
         self.new_pointer = None
         self.binary_text = None
         self.length = 0
-        self.original_text = original_text
+        self.max_length = max_length
 
     def prepare(self):
         words = self.text.split()
@@ -38,7 +42,7 @@ class TextString:
         self.length = 0
         line_num = 0
         for word in words:
-            if len(word) + cur_line_length + 1 >= MAX_LENGTH:
+            if len(word) + cur_line_length + 1 >= self.max_length:
                 if line_num % 2 == 0:
                     prepared_text += "<FE>"
                 else:
@@ -66,19 +70,45 @@ table = None
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='1.0')
 
-    if (arguments['<tablefile>']):
-        table = TranslationTable(arguments['<tablefile>'])
+    table = TranslationTable(arguments['<tablefile>'])
+
+
+    if not arguments["--no-backup"]:
+        # Make a backup of the rom file in case...
+        shutil.copy(arguments["<romfile>"], arguments["<romfile>"] + ".backup." + datetime.datetime.now().strftime(format="%Y%m%d_%H_%M_%S"))
 
     rom = open(arguments["<romfile>"], 'rb+')
 
+    translation_file = open(arguments["<inputfile>"], encoding='utf-8')
+    data = yaml.load(translation_file, Loader=yaml.FullLoader)
+
+    script = data["script"]
+    combat = data["combat"]
+    combat_wide = data["combat_wide"]
+
     messages = [
-        #TextString(0x26C35, '''Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz , . ' " ? !''' )
-        TextString(0x1A581, "A morning in the Jungle.<FC>", "ジャングルの あさが きました.<FC>"),
-        TextString(0xD90A, "Jungle Dad 「Good morning <var0>!", "ジャングルパパ<FE>「おはよう! <var0>.<FF>"),
-        TextString(0xD910, "「Hey hey why do you not at least put on some pants?<FD>「They are in that box check it out!", "「こらこら パンツぐらい<FE> はいて きなさい.<FD>「その ハコの なかに あるから<FE> しらべて ごらん.<FF>"),
-        TextString(0xD919, "「Even if you grabbed a pair of pants you cannot just hold on to them you know?<FD>「You also have to wear them!", "「パンツを とってきても<FE> もってるだけじゃ ダメじゃないか.<FD>「そうびして みに つけなくちゃね.<FF>"),
-        TextString(0xD922, "「Alright. Then let us walk around the jungle this morning!<FD>「It seems there are bad guys running around. I have got to keep my eyes open and watch out for them.")
+        TextString(0x1A581,
+                   "A morning in the Jungle.<FC>")
     ]
+
+    for m in combat:
+        if m["translation"][0:4] != 'TODO' and m['pointer_location'] != 0:
+            messages.append(TextString(m['pointer_location'],
+                                       m['translation'],
+                                       max_length=10)
+                            )
+
+    for m in combat_wide:
+        if m["translation"][0:4] != 'TODO' and m['pointer_location'] != 0:
+            messages.append(TextString(m['pointer_location'],
+                                       m['translation'])
+                            )
+
+    for m in script:
+        if m["translation"][0:4] != 'TODO' and m['pointer_location'] != 0:
+            messages.append(TextString(m['pointer_location'],
+                                       m['translation'])
+                            )
 
     message_index = 0
     total_length = 0
@@ -90,9 +120,7 @@ if __name__ == '__main__':
         m.new_pointer = total_length.to_bytes(2, "little")
         total_length += m.length
         rom.seek(m.pointer_address)
-        print("Pointer address : " + str(hex(m.pointer_address)))
         rom.write((message_index * 3).to_bytes(2, "little"))
-        print(str((message_index * 3).to_bytes(2, "little")))
 
         rom.seek(0x10 * 0x4000 + 0x1000 + message_index * 0x03)
         rom.write(m.new_bank)
@@ -103,6 +131,7 @@ if __name__ == '__main__':
         message_index += 1
 
     rom.close()
+
 # Need:
 #   The old pointer adress (where the pointer is in the ROM)
 #   The new script
