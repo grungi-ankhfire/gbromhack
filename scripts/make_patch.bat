4c6dacea7d1103e06e57f9a8f43bd49ad447db3c
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2020-06-24 23:11:21 +0200

    Update translation to 0.2.0

--- scripts/make_patch.bat
+++ scripts/make_patch.bat
@@ -1,2 +1,2 @@
 rgbfix -v -p 0x00 roms/jw_patched.gb
-ips_util create roms/jw_original.gb roms/jw_patched.gb -o JungleWars_EnglishPatch_0.1.ips
+ips_util create roms/jw_original.gb roms/jw_patched.gb -o JungleWars_EnglishPatch_0.2.0.ips
